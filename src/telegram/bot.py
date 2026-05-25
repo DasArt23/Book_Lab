1ce128deb7b53bb59e749449ef723d87ff5c47b7
dasart_deb <dasart@dasart.beeline>
2026-05-25 03:28:25 +0300

    Добавлено: добавления источника Random

--- src/telegram/bot.py
+++ src/telegram/bot.py
@@ -1,7 +1,9 @@
 import os
+import asyncio
 from aiogram import Bot, Dispatcher
 from domain.constants import TOKEN
 import telegram.handlers.commands as commands
+import telegram.handlers.parsing as parsing
 from aiohttp import web
 
 
@@ -9,15 +11,19 @@ async def handle(request):
     return web.Response(text="Bot is alive")
 
 
-async def start_fake_server():
+async def run_web_server():
     app = web.Application()
     app.router.add_get('/', handle)
+
     runner = web.AppRunner(app)
     await runner.setup()
+
     port = int(os.getenv("PORT", 10000))
     site = web.TCPSite(runner, "0.0.0.0", port)
     await site.start()
 
+    while True:
+        await asyncio.sleep(3600)
 
 async def on_shutdown(dispatcher: Dispatcher, bot: Bot):
     await bot.session.close()
@@ -26,12 +32,15 @@ async def on_shutdown(dispatcher: Dispatcher, bot: Bot):
 async def run_bot():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
-    dp.include_routers(commands.router)
-
+    dp.include_routers(commands.router, parsing.router)
     dp.shutdown.register(on_shutdown)
 
-    # Запускаем веб-сервер, чтобы Render одобрил деплой
-    await start_fake_server()
-
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, polling_timeout=10)
+
+    print("Запуск процессов...")
+
+    # Запускаем обе задачи параллельно. Теперь они делят Event Loop поровну.
+    await asyncio.gather(
+        run_web_server(),
+        dp.start_polling(bot, polling_timeout=10)
+    )
---
+++ src/telegram/keyboards/menu.py
@@ -0,0 +1,26 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from data_processing.fabrics import Sources_factory
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+def get_start_menu():
+    buttons = [
+            KeyboardButton(text="Получить книги"),
+    ]
+    return ReplyKeyboardMarkup(
+        keyboard=buttons,
+        resize_keyboard=True,
+        input_field_placeholder="Добавьте источники книг"
+    )
+
+
+def add_sources():
+    builder = ReplyKeyboardBuilder()
+    names = Sources_factory._sources.keys()
+    for source_name in names:
+        builder.add(KeyboardButton(text=source_name))
+    builder.adjust(2)
+    builder.add(KeyboardButton(text="Хватит"))
+    builder.add(KeyboardButton(text="Очистить"))
+    builder.add(KeyboardButton(text="Просмотреть"))
+    return builder.as_markup(resize_keyboard=True)
