d8624b0d20e5b289a6d0e94574cd56c622897d80
dasart_deb <dasart@dasart.beeline>
2026-05-25 05:16:36 +0300

    Телеграмм бот

--- src/main.py
+++ src/main.py
@@ -2,31 +2,22 @@ from application import Application
 from data_processing.fabrics import Sources_factory, Handler_factory
 from config import AppConfig, ExecutionMode
 import asyncio
-import telegram.bot
 
 
 def main():
     conf = AppConfig()
-
-    handler = Handler_factory.get_handler(
-        conf.handler_type,
-        **conf.handler_param,
-    )
-
-    sources = (
-        Sources_factory.get_source(**src_conf)
-        for src_conf in conf.get_sources()
-    )
-
+    handler = Handler_factory.get_handler(conf.handler_type, **conf.handler_param)
+    sources = [Sources_factory.get_source(**src_conf) for src_conf in conf.get_sources()]
     app = Application(sources, handler)
+
     if conf.is_async_mode:
         asyncio.run(app.run_async())
     elif conf.execution_mode in [ExecutionMode.THREAD, ExecutionMode.PROCESS]:
         asyncio.run(app.run_hybrid())
-    elif conf.execution_mode == ExecutionMode.TELEG:
-        asyncio.run(telegram.bot.run_bot())
     else:
         app.run()
+
+    return app
 
 
 if __name__ == "__main__":
